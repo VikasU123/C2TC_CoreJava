926e52868571c4fb8730e1777b6b896c1d266823
VikasU123 <vikaschinnu1111@gmail.com>
2022-06-03 06:49:27 +0530

    first commit

---
+++ Day4/src/ClassInstantiation2.java
@@ -0,0 +1,15 @@
+
+public class ClassInstantiation2 {
+	int x;
+
+	public static void main(String[] args) {
+		ClassInstantiation2 myObj1 = new ClassInstantiation2();
+		ClassInstantiation2 myObj2 = new ClassInstantiation2();
+		myObj1.x = 24;
+		myObj2.x = 55;
+		System.out.println(myObj1.x);
+		System.out.println(myObj2.x);
+
+	}
+
+}
