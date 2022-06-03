c0639aec5d2a447c94049f7928749f8e92bc36e9
VikasU123 <vikaschinnu1111@gmail.com>
2022-06-03 07:08:34 +0530

    first commit
    Class instantiation without giving values to the variables (String, float, double, char, long)

---
+++ Day4/src/ClassInstantiation4.java
@@ -0,0 +1,26 @@
+
+public class ClassInstantiation4 {
+	String x;
+	float y;
+	char z;
+	double p;
+	float q;
+	long r;
+
+	public static void main(String[] args) {
+		ClassInstantiation4 myObj1 = new ClassInstantiation4();
+		ClassInstantiation4 myObj2 = new ClassInstantiation4();
+		ClassInstantiation4 myObj3 = new ClassInstantiation4();
+		ClassInstantiation4 myObj4 = new ClassInstantiation4();
+		ClassInstantiation4 myObj5 = new ClassInstantiation4();
+		ClassInstantiation4 myObj6 = new ClassInstantiation4();
+		System.out.println(myObj1.x);
+		System.out.println(myObj2.y);
+		System.out.println(myObj3.z);
+		System.out.println(myObj4.p);
+		System.out.println(myObj5.q);
+		System.out.println(myObj6.r);
+
+	}
+
+}
