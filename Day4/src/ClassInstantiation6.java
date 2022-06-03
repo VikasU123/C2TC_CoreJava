699b70ce6eb385732b3c1b72f2057d0c3b270d45
VikasU123 <vikaschinnu1111@gmail.com>
2022-06-03 07:32:38 +0530

    first commit
    Class Instantiation example using several instance variables and only one object

---
+++ Day4/src/ClassInstantiation6.java
@@ -0,0 +1,19 @@
+
+public class ClassInstantiation6{
+			int x;
+			int y;
+			int z;
+
+			public static void main(String[] args) {
+				ClassInstantiation6 myObj = new ClassInstantiation6();
+				myObj.x=1;
+				myObj.y=2;
+				myObj.z=3;
+				System.out.println(myObj.x);
+				System.out.println(myObj.y);
+				System.out.println(myObj.z);
+
+
+	}
+
+}
