f900d433468485f8dabacd85d6d6028d1e94031a
VikasU123 <vikaschinnu1111@gmail.com>
2022-06-03 06:56:18 +0530

    first commit
    Class Instantiation example without giving value to the variables to check the   default value(int)

---
+++ Day4/src/ClassInstantiation3.java
@@ -0,0 +1,13 @@
+
+public class ClassInstantiation3 {
+	int x;
+
+	public static void main(String[] args) {
+		ClassInstantiation3 myObj1  = new ClassInstantiation3();
+		ClassInstantiation3 myObj2 = new ClassInstantiation3();
+		System.out.println(myObj1.x);
+		System.out.println(myObj2.x);
+
+	}
+
+}
