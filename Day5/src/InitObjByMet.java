dd88856fa2be83fd0e287236da969f3aa94d7387
VikasU123 <vikaschinnu1111@gmail.com>
2022-06-04 14:51:07 +0530

    first commit

---
+++ Day5/src/InitObjByMet.java
@@ -0,0 +1,24 @@
+public class InitObjByMet {
+	int rollno;
+	String name;
+	void insertRecord(int r, String n) {
+		rollno = r;
+		name = n;
+	}
+	void displayInformation() {
+		System.out.println(rollno+" "+name);
+	}
+}
+
+class Student{
+	public static void main(String[] args) {
+		InitObjByMet s1 = new InitObjByMet();
+		InitObjByMet s2 = new InitObjByMet();
+		s1.insertRecord(111, "vikas");
+	    s2.insertRecord(222, "sanjay");
+	    s1.displayInformation();
+	    s2.displayInformation();
+
+	}
+
+}
---
+++ Day5/src/InitObjByRefVar.java
@@ -0,0 +1,15 @@
+public class InitObjByRefVar {
+	int id;
+	String name;
+}
+
+class Main{
+	public static void main(String[] args) {
+		InitObjByRefVar s1 = new InitObjByRefVar();
+		s1.id = 72;
+		s1.name = "Vikas";
+		System.out.println(s1.id+" "+s1.name);
+
+	}
+
+}
