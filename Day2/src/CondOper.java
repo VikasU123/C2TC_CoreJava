80f204f1f8090fbd6b2b029587d22ce1879bd294
VikasU123 <vikaschinnu1111@gmail.com>
2022-06-06 22:11:59 +0530

    first commit

---
+++ Day2/src/CondOper.java
@@ -0,0 +1,14 @@
+
+public class CondOper {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int value1 = (a<b)?a:b;
+		int value2 = (a>b)?a:b;
+		System.out.println(value1);
+		System.out.println(value2);
+
+	}
+
+}
---
+++ Day3/src/ElseIfSta.java
@@ -0,0 +1,18 @@
+
+public class ElseIfSta {
+
+	public static void main(String[] args) {
+		String lang = "Java";
+		if(lang== "python") {
+			System.out.println("language is python");
+		}else if(lang=="C++") {
+			System.out.println("lang is C++");
+		}else if(lang=="java") {
+			System.out.println("lang is java");
+		}else {
+			System.out.println(lang);
+		}
+
+	}
+
+}
